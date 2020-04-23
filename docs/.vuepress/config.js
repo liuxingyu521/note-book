1991ab6175dfca0f19445d299cc7d5ed452f5de5
liuxingyu521 <xuj_power@qq.com>
2020-04-23 16:50:07 +0800

    feat: 增加编辑链接

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,10 @@ module.exports = {
       '/',
       '/babel/',
     ],
-    lastUpdated: "最后更新时间"
+    lastUpdated: "最后更新时间",
+    editLinks: true,
+    editLinkText: '在 Github 上编辑此页',
+    repo: 'https://github.com/liuxingyu521/note-book',
+    docsDir: 'docs',
   }
 }
